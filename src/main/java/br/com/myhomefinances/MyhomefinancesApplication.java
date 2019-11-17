2be33f543f434beb5c8cbd4b2a32d8b4b8e5b4d7
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-17 00:43:08 -0300

    Criação do projeto maven

---
+++ src/main/java/br/com/myhomefinances/MyhomefinancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.myhomefinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomefinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomefinancesApplication.class, args);
+	}
+
+}
