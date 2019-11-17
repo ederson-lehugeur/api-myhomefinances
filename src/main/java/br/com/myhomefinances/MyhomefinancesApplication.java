e5f2c7961ed8a9c3de64b39b476a7ea23abe9154
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-17 01:54:28 -0300

    Criando operação de instanciação para categoria

--- src/main/java/br/com/myhomefinances/MyhomefinancesApplication.java
+++ src/main/java/br/com/myhomefinances/MyhomefinancesApplication.java
@@ -1,13 +1,33 @@
 package br.com.myhomefinances;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.myhomefinances.domain.Categoria;
+import br.com.myhomefinances.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class MyhomefinancesApplication {
+public class MyhomefinancesApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyhomefinancesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria categoria1 = new Categoria(null, "Operação Bancária",
+				"Operações de Saque, Depósito, Transferências, etc.");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1));
+
 	}
 
 }
