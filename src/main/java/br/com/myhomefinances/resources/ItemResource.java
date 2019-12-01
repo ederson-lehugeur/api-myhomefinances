d384f2edf198ad84acd02bfd068e623c5742c811
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-30 22:58:59 -0300

    Paginação com parâmetros opcionais na requisição do endpoint de itens

--- src/main/java/br/com/myhomefinances/resources/ItemResource.java
+++ src/main/java/br/com/myhomefinances/resources/ItemResource.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,6 +41,18 @@ public class ItemResource {
 		Item item = itemService.find(id);
 
 		return ResponseEntity.ok().body(item);
+	}
+
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<Item>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+
+		Page<Item> listaItens = itemService.findPage(page, linesPerPage, orderBy, direction);
+
+		return ResponseEntity.ok().body(listaItens);
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
--- src/main/java/br/com/myhomefinances/services/ItemService.java
+++ src/main/java/br/com/myhomefinances/services/ItemService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.myhomefinances.domain.Categoria;
@@ -29,6 +32,12 @@ public class ItemService {
 
 		return item.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!",
 				id, Item.class.getName()));
+	}
+
+	public Page<Item> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return itemRepository.findAll(pageRequest);
 	}
 
 	public Item insert(Item item) {
