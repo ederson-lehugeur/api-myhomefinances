10c482178087cebe7bcb0d44509bee7bb0277eb3
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-12-18 22:37:02 -0300

    Refresh token

---
+++ src/main/java/br/com/myhomefinances/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package br.com.myhomefinances.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.myhomefinances.security.JWTUtil;
+import br.com.myhomefinances.security.UserDetailsSpringSecurity;
+import br.com.myhomefinances.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value="/refreshToken", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserDetailsSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+
+		return ResponseEntity.noContent().build();
+	}
+}
